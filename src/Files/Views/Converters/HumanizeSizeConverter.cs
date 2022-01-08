1b9af1bebe1bdccc86667db2274d47d9300e0e76
appleneko2001 <appleneko2001@outlook.com>
2022-01-09 06:15:48 +0900

    Implement humanized size converter

---
+++ src/Files/Views/Converters/HumanizeSizeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Files.Extensions;
+
+namespace Files.Views.Converters
+{
+    public class HumanizeSizeConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is long l)
+            {
+                return l.HumanizeSizeString(culture);
+            }
+
+            return null;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
