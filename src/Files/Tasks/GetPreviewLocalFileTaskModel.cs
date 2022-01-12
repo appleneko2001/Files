bebbf3ff23fabeb9d8d0ce22b9a80f6c74abf722
appleneko2001 <appleneko2001@outlook.com>
2022-01-12 20:51:57 +0900

    Implement get preview task model for use

---
+++ src/Files/Tasks/GetPreviewLocalFileTaskModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Files.Tasks
+{
+    public class GetPreviewLocalFileTaskModel : GetPreviewTaskModel
+    {
+        public FileInfo LocalFileInfo;
+        
+        public override Stream GetStream()
+        {
+            if (LocalFileInfo is null)
+                throw new ArgumentNullException(nameof(LocalFileInfo));
+
+            return LocalFileInfo.OpenRead();
+        }
+    }
+}
---
+++ src/Files/Tasks/GetPreviewTaskModel.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using Files.Views.Models.Browser.Preview;
+
+namespace Files.Tasks
+{
+    public abstract class GetPreviewTaskModel : TaskResultModel<PreviewableViewModelBase>
+    {
+        public int TargetPreviewSize = 160;
+        
+        public abstract Stream GetStream();
+    }
+}
