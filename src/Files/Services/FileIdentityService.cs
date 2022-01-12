1569ab6e54ce26cef7e2bd24a8ca2019e6024276
appleneko2001 <appleneko2001@outlook.com>
2022-01-12 21:14:44 +0900

    Add package FileSignatures and add determine logic

--- src/Files/Services/FileIdentityService.cs
+++ src/Files/Services/FileIdentityService.cs
@@ -1,7 +1,43 @@
-﻿namespace Files.Services
+﻿using System.IO;
+using FileSignatures;
+using FileSignatures.Formats;
+
+namespace Files.Services
 {
     public class FileIdentityService
     {
+        private static FileFormatInspector _inspectorInstance;
         
+        static FileIdentityService()
+        {
+            _inspectorInstance = new FileFormatInspector();
+        }
+        
+        public static bool DetermineFilePreviewable(FileInfo fi)
+        {
+            using (var stream = fi.OpenRead())
+            {
+                return DetermineFilePreviewable(stream);
+            }
+        }
+        
+        public static bool DetermineFilePreviewable(Stream stream)
+        {
+            var format = _inspectorInstance.DetermineFileFormat(stream);
+
+            switch (format)
+            {
+                case Png:
+                case JpegJfif:
+                case JpegExif:
+                case Jpeg:
+                case Gif:
+                case Bmp:
+                case Image:
+                    return true;
+            }
+
+            return false;
+        }
     }
 }
