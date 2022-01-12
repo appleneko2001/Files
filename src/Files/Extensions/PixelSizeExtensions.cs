6421f152353d2ade8f4099cca6453e9ffa3720da
appleneko2001 <appleneko2001@outlook.com>
2022-01-12 21:27:08 +0900

    Implement extensions for pixel size struct

---
+++ src/Files/Extensions/PixelSizeExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+
+namespace Files.Extensions
+{
+    public static class PixelSizeExtensions
+    {
+        public static bool GreaterThan(this PixelSize a, PixelSize b)
+        {
+            return a.Width > b.Width && a.Height > b.Height;
+        }
+        
+        public static bool GreaterEqualThan(this PixelSize a, PixelSize b)
+        {
+            return a.Width >= b.Width && a.Height >= b.Height;
+        }
+        
+        public static bool LessThan(this PixelSize a, PixelSize b)
+        {
+            return a.Width < b.Width && a.Height < b.Height;
+        }
+        
+        public static bool LessEqualThan(this PixelSize a, PixelSize b)
+        {
+            return a.Width <= b.Width && a.Height <= b.Height;
+        }
+    }
+}
