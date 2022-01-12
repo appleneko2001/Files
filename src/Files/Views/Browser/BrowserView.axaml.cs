d069aca875d1201f37020f7c26deecb7cb0d1e12
appleneko2001 <appleneko2001@outlook.com>
2022-01-12 21:01:41 +0900

    Update BrowserView design and code-behind logic

--- src/Files/Views/Browser/BrowserView.axaml.cs
+++ src/Files/Views/Browser/BrowserView.axaml.cs
@@ -1,5 +1,9 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Files.Views.Controls.Events;
+using Files.Views.Models;
 using Files.Views.Models.Browser.Files.Local;
+using Files.Views.Models.Browser.Preview;
 
 namespace Files.Views.Browser
 {
@@ -16,6 +20,27 @@ namespace Files.Views.Browser
                     e.TemplateKey = "Folder";
                     break;
             }
+        }
+
+        private void SelectingItemRepeater_OnDoubleTappedItemEvent(object sender, AdditionalEventArgs e)
+        {
+            if(e.Argument is not Visual v)
+                return;
+
+            if (v.DataContext is not ItemViewModelBase vm)
+                return;
+
+            if (vm.OnClickCommand == null)
+                return;
+            
+            if(vm.OnClickCommand.CanExecute(vm))
+                vm.OnClickCommand.Execute(vm);
+        }
+
+        private void BrowserViewPreviewElement_OnSelectTemplateKey(object sender, SelectTemplateEventArgs e)
+        {
+            if (e.DataContext is PreviewableViewModelBase)
+                e.TemplateKey = "Image";
         }
     }
 }
