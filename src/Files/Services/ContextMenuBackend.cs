29a4cbf12b486c046065885a2b0f70dec5e176e4
appleneko2001 <appleneko2001@outlook.com>
2022-01-15 21:40:35 +0900

    Create context menu for different kind (file or folder)

--- src/Files/Services/ContextMenuBackend.cs
+++ src/Files/Services/ContextMenuBackend.cs
@@ -19,7 +19,7 @@ namespace Files.Services
             AddOrModifyResource(app.Resources, FileContextMenuResourceName,
                 InitiateFileContextMenus());
             AddOrModifyResource(app.Resources, FolderContextMenuResourceName,
-                InitiateFileContextMenus());
+                InitiateFolderContextMenus());
         }
 
         private static void AddOrModifyResource(IResourceDictionary dict, object key, object? value)
@@ -32,9 +32,33 @@ namespace Files.Services
         {
             var list = new List<ContextMenuItemViewModelBase>();
             
+            list.Add(new ContextMenuItemViewModel("Open", new MaterialIconViewModel(MaterialIconKind.Launch)));
+            list.Add(new ContextMenuItemViewModel("Open with..", new MaterialIconViewModel(MaterialIconKind.Launch)));
+            list.AddRange(InitiatePostEssentialMenus());
+
+            return list;
+        }
+        
+        private static IEnumerable<ContextMenuItemViewModelBase> InitiateFolderContextMenus()
+        {
+            var list = new List<ContextMenuItemViewModelBase>();
+            
+            list.Add(new ContextMenuItemViewModel("Open folder"));
+            list.AddRange(InitiatePostEssentialMenus());
+
+            return list;
+        }
+        
+        private static IEnumerable<ContextMenuItemViewModelBase> InitiatePostEssentialMenus()
+        {
+            var list = new List<ContextMenuItemViewModelBase>();
+            
             list.Add(new ContextMenuItemViewModel("Cut", new MaterialIconViewModel(MaterialIconKind.ContentCut)));
             list.Add(new ContextMenuItemViewModel("Copy", new MaterialIconViewModel(MaterialIconKind.ContentCopy)));
             list.Add(new ContextMenuItemViewModel("Paste", new MaterialIconViewModel(MaterialIconKind.ContentPaste)));
+            list.Add(new ContextMenuItemViewModel("-"));
+            list.Add(new ContextMenuItemViewModel("Delete", new MaterialIconViewModel(MaterialIconKind.TrashCan)));
+            list.Add(new ContextMenuItemViewModel("Rename", new MaterialIconViewModel(MaterialIconKind.Pencil)));
             list.Add(new ContextMenuItemViewModel("-"));
             list.Add(new ContextMenuItemViewModel("Properties"));
 
