dbd91d45c72e0dca9fc1ae0ec1969237b1ef99f9
appleneko2001 <appleneko2001@outlook.com>
2022-01-19 01:34:52 +0900

    Implement header view model base for further use

---
+++ src/Files/Views/Models/HeaderViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿namespace Files.Views.Models
+{
+    public class HeaderViewModelBase : ViewModelBase
+    {
+        private object? _icon;
+        public object? Icon
+        {
+            get => _icon;
+            protected set
+            {
+                _icon = value;
+                RaiseOnPropertyChanged();
+            }
+        }
+        
+        private string _header;
+        public string Header
+        {
+            get => _header;
+            protected set
+            {
+                _header = value;
+                RaiseOnPropertyChanged();
+            }
+        }
+    }
+}
