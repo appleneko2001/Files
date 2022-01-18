f520d83e7ce7ddc9a20c30304cc6aeadd3a2f8d6
appleneko2001 <appleneko2001@outlook.com>
2022-01-19 01:43:50 +0900

    Forgot to upload commit: Fullscreen command implement

--- src/Files/Views/Models/BrowserWindowViewModel.cs
+++ src/Files/Views/Models/BrowserWindowViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using Avalonia;
+using Avalonia.Controls;
+using Files.Commands;
 using Files.Services;
 using Files.Views.Models.Storage;
 
@@ -8,6 +10,14 @@ namespace Files.Views.Models
 {
     public class BrowserWindowViewModel : ViewModelBase
     {
+        private static RelayCommand _fullscreenCommand = new RelayCommand(delegate(object o)
+        {
+            if (o is Window w)
+            {
+                w.WindowState = w.WindowState == WindowState.FullScreen ? WindowState.Normal : WindowState.FullScreen;
+            }
+        });
+        
         private BrowserWindow _parentWindow;
 
         private AppBackend _context;
@@ -21,6 +31,8 @@ namespace Files.Views.Models
         
         public ObservableCollection<StorageDeviceViewModel> StorageDevices => _storageDevices;
         public ObservableCollection<BrowserWindowTabViewModel> TabViewModel => _tabsViewModel;
+
+        public RelayCommand FullscreenCommand => _fullscreenCommand;
 
         public BrowserWindowTabViewModel SelectedTab
         {
