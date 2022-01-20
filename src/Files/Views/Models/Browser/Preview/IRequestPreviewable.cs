bd210ace0192b782221fb26e9b51f7709a002464
appleneko2001 <appleneko2001@outlook.com>
2022-01-20 16:00:36 +0900

    Implement interface for provide request preview method

---
+++ src/Files/Views/Models/Browser/Preview/IRequestPreviewable.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+
+namespace Files.Views.Models.Browser.Preview
+{
+    public interface IRequestPreviewable
+    {
+        void TryGetPreview(CancellationToken _cancellationToken = default);
+    }
+}
