846457b222107a1fa33ed5883c3fe2a63c82dc57
appleneko2001 <appleneko2001@outlook.com>
2022-01-25 22:36:08 +0900

    Implement string format converter for further use

---
+++ src/Files/Views/Converters/StringFormatConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Files.Views.Converters
+{
+    public class StringFormatConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return parameter is not string format ? value : string.Format(format, value);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
