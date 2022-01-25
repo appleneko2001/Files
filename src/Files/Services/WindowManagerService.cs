3ca78664814e82813b9969d733b17a13be150e15
appleneko2001 <appleneko2001@outlook.com>
2022-01-25 22:51:38 +0900

    Code formation, add LastFocusedWindow property (WindowManagerService)

--- src/Files/Services/WindowManagerService.cs
+++ src/Files/Services/WindowManagerService.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Files.Views;
 
 namespace Files.Services
 {
     public class WindowManagerService
     {
+        public event EventHandler WhenNoActiveWindowsLeft;
+        
+        private ObservableCollection<Window> _activeWindows;
+        private Window? _lastFocusedWindow;
+        private bool _isLockedDown;
+        
+        public Window? LastFocusedWindow => _lastFocusedWindow;
+        
         public WindowManagerService()
         {
             _activeWindows = new ObservableCollection<Window>();
-            
         }
 
         public bool RegisterWindow(Window inst)
@@ -20,17 +29,25 @@ namespace Files.Services
             if (_activeWindows.Contains(inst))
                 return false;
             
+            inst.GotFocus += OnAvaloniaWindowGotFocus;
             inst.Closed += OnAvaloniaWindowClosed;
             _activeWindows.Add(inst);
             return true;
         }
 
-        public event EventHandler WhenNoActiveWindowsLeft;
+        private void OnAvaloniaWindowGotFocus(object sender, GotFocusEventArgs e)
+        {
+            if(sender is not BrowserWindow window)
+                return;
+
+            _lastFocusedWindow = window;
+        }
 
         private void OnAvaloniaWindowClosed(object sender, EventArgs e)
         {
             if (sender is Window window)
             {
+                window.GotFocus -= OnAvaloniaWindowGotFocus;
                 _activeWindows.Remove(window);
             }
 
@@ -39,8 +56,5 @@ namespace Files.Services
                 WhenNoActiveWindowsLeft?.Invoke(this, EventArgs.Empty);
             }
         }
-
-        private ObservableCollection<Window> _activeWindows;
-        private bool _isLockedDown;
     }
 }
