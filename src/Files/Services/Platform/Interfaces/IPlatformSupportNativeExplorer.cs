904bd320604e072412fd40dfedccf42f4df65f2f
appleneko2001 <appleneko2001@outlook.com>
2022-01-25 22:41:54 +0900

    Add invoke native file explorer interface

---
+++ src/Files/Services/Platform/Interfaces/IPlatformSupportNativeExplorer.cs
@@ -0,0 +1,9 @@
+﻿namespace Files.Services.Platform.Interfaces
+{
+    public interface IPlatformSupportNativeExplorer
+    {
+        string NativeExplorerName { get; }
+        
+        void OpenFolderWithNativeExplorer(string path);
+    }
+}
