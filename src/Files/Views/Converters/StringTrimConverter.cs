6400de71c3155db846c678ab03aa9a1111bfc1da
appleneko2001 <appleneko2001@outlook.com>
2022-04-13 23:00:24 +0900

    Update BrowserDrawer (Use binding instead, entry display text trimming)

---
+++ src/Files/Views/Converters/StringTrimConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Files.Views.Converters
+{
+    public class StringTrimConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            // Keep end of string with ellipsis if it is too long
+
+            if (value is not string str)
+                return value;
+            
+            const int maxLength = 20;
+
+            if (str.Length <= maxLength)
+                return str;
+            
+            var offset = str.Length - maxLength;
+            return "..." + str[offset..];
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
