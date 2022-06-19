f9ef948a7c57883bfb80d76f69f120e5246f0529
appleneko2001 <appleneko2001@outlook.com>
2022-06-19 19:44:03 +0900

    Update adb sync command interface

---
+++ src/Files.Adb/Commands/Sync/IAdbSyncCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace Files.Adb.Commands.Sync
+{
+    public interface IAdbSyncCommand
+    {
+        public AdbStream AdbStream { get; set; }
+        
+        IAsyncEnumerable<AdbSyncCommandResultBase> RunAsync(IDictionary<string, object> args,
+            CancellationToken cancellationToken);
+    }
+
+    public interface IAdbSyncCommand<out TResult> : IAdbSyncCommand where TResult : AdbSyncCommandResultBase 
+    {
+        new IAsyncEnumerable<TResult> RunAsync(IDictionary<string, object> args,
+            CancellationToken cancellationToken);
+    }
+}
