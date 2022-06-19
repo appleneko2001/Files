7cc78b89e9dd14810e1bb34e307277508810fa2b
appleneko2001 <appleneko2001@outlook.com>
2022-06-19 19:11:02 +0900

    add AdbProtocolConstants.cs, used for store parameters for adb server

---
+++ src/Files.Adb/Apis/AdbProtocolConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Files.Adb.Apis
+{
+    public class AdbProtocolConstants
+    {
+        // Official ADB will receive and send data packet with buffer size header.
+        // by default it should be 4, that means buffer size is Int32.
+        public const int AdbProtocolSizeHeader = 4;
+
+        // Character length should match AdbProtocolSizeHeader
+        public const string AdbNumericToHexFormat = "X4";
+    }
+}
