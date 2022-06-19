9a94df6778e34b00f35a6ac1f94663575151c2e4
appleneko2001 <appleneko2001@outlook.com>
2022-06-19 20:47:36 +0900

    Add ConvertBack implementation on BoolToDoubleConverter

--- src/Files/Views/Converters/BoolToDoubleConverter.cs
+++ src/Files/Views/Converters/BoolToDoubleConverter.cs
@@ -35,7 +35,12 @@ namespace Files.Views.Converters
 
         public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value is double d)
+            {
+                return d > 0.5;
+            }
+
+            return false;
         }
     }
 }
