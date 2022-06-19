6588431c2b266cd5d953ee89e2d26a55ca3e8a24
appleneko2001 <appleneko2001@outlook.com>
2022-06-19 19:51:48 +0900

    add AdbCommandResult.cs

---
+++ src/Files.Adb/Commands/Results/AdbCommandResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Files.Adb.Commands.Results
+{
+    public class AdbCommandResult
+    {
+        public bool IsFailed { get; set; }
+        public bool IsSuccess { get; set; }
+        public double Progress { get; set; }
+        public Exception Exception { get; set; }
+    }
+    
+    public class AdbCommandResult<T> : AdbCommandResult
+    {
+        public T Result { get; set; }
+    }
+}
