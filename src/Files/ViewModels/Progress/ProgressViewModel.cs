e82e95a99fbb4f01f8f0dae07814613d033f81d1
appleneko2001 <appleneko2001@outlook.com>
2022-06-19 20:03:03 +0900

    Update ProgressViewModel (add cancel command shortcut)

--- src/Files/ViewModels/Progress/ProgressViewModel.cs
+++ src/Files/ViewModels/Progress/ProgressViewModel.cs
@@ -1,15 +1,35 @@
-﻿using MinimalMvvm.ViewModels;
+﻿using System.Threading;
+using MinimalMvvm.ViewModels;
+using MinimalMvvm.ViewModels.Commands;
 
 namespace Files.ViewModels.Progress
 {
     public class ProgressViewModel : ViewModelBase
     {
+        private static readonly RelayCommand _cancelCommand = new (CancelCommand_Execute);
+
+        private static void CancelCommand_Execute(object? obj)
+        {
+            if (obj is not ProgressViewModel vm)
+                return;
+            
+            vm.TryCancel();
+        }
+
+
         private bool _isComplete;
         private double? _progress;
+        private readonly CancellationTokenSource? _cancellationTokenSource;
+
+        public ProgressViewModel(CancellationTokenSource? cancellationTokenSource = default)
+        {
+            _cancellationTokenSource = cancellationTokenSource;
+        }
 
         public bool IsComplete => _isComplete;
         public bool IsIndeterminate => _progress == null;
         public double Progress => _progress ?? 0.0;
+        public RelayCommand CancelCommand => _cancelCommand;
 
         public void SetCompleted()
         {
@@ -22,6 +42,11 @@ namespace Files.ViewModels.Progress
             _progress = p;
             OnPropertyChanged(nameof(IsIndeterminate));
             OnPropertyChanged(nameof(Progress));
+        }
+
+        public void TryCancel()
+        {
+            _cancellationTokenSource?.Cancel();
         }
     }
 }
